9fd29d91f17248a4bdc550535c02ef61dde7ad1f
kanha55266442 <39862744+kanha55266442@users.noreply.github.com>
2019-11-29 21:25:17 +0530

    Added BaseEntity closes#10

---
+++ PetClinic/Pet-Clinic-Data/src/main/java/org/Pet/Clinic/Data/model/BaseEntity.java
@@ -0,0 +1,22 @@
+package org.Pet.Clinic.Data.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
