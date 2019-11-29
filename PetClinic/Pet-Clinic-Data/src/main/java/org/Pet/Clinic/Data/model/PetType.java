950a01cb35e5467f14a3b2d85c503e5d926b8c8e
kanha55266442 <39862744+kanha55266442@users.noreply.github.com>
2019-11-29 21:27:50 +0530

    .

--- PetClinic/Pet-Clinic-Data/src/main/java/org/Pet/Clinic/Data/model/PetType.java
+++ PetClinic/Pet-Clinic-Data/src/main/java/org/Pet/Clinic/Data/model/PetType.java
@@ -1,6 +1,6 @@
 package org.Pet.Clinic.Data.model;
 
-public class PetType extends BaseEntity{
+public class PetType extends BaseEntity {
     private String name;
 
     public PetType(String name) {
